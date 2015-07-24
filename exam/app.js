b7a38300f9197157b6f7838c200e2f9af085345d
unknown <test29239@gmail.com>
2015-07-24 10:41:50 +0530

    files commiting

--- exam/app.js
+++ exam/app.js
@@ -1,60 +1,119 @@
 var express = require('express');
+var http = require('http');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/exam');
+var db = require('./schema/db');
+var db1 = require('./schema/questionschema');
+var admin = require('./controllers/admin');
+var user_det = require('./controllers/user');
+var swig=require('swig');
+var session = require('express-session');
+//==================================================================
+// Define the strategy to be used by PassportJS
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    if (username === "admin" && password === "admin") // stupid example
+      return done(null, {name: "admin"});
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+    return done(null, false, { message: 'Incorrect username.' });
+  }
+));
 
+// Serialized and deserialized methods when got from session
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+// Define a middleware function to be used for every secured routes
+var auth = function(req, res, next){
+  if (!req.isAuthenticated()) 
+    res.send(401);
+  else
+    next();
+};
+//==================================================================
+
+// Start express application
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.engine('html', require('swig').renderFile);
+app.set('view engine', 'html');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.cookieParser()); 
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.session({ secret: 'securedsession' }));
+app.use(passport.initialize()); // Add passport initialization
+app.use(passport.session());    // Add passport initialization
+app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+app.use(session({secret: 'ssshhhhh'}));
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+//==================================================================
+// routes
+app.get('/', function(req, res){
+  //console.log(req.session.username+'sessions et')
+  res.render('index', { title: 'Express' });
+});
+
+app.get('/users', auth, function(req, res){
+  res.send([{name: "user1"}, {name: "user2"}]);
+});
+//==================================================================
+
+//==================================================================
+// route to test if the user is logged in or not
+app.get('/loggedin', function(req, res) {
+  res.send(req.isAuthenticated() ? req.user : '0');
+});
+
+// route to log in
+app.post('/login', passport.authenticate('local'), function(req, res) {
+  res.send(req.user);
+});
+
+// route to log out
+app.post('/logout', function(req, res){
+  req.logOut();
+  res.send(200);
 });
 
 
-module.exports = app;
+app.get('/addCategory', admin.addcategory);
+app.post('/deleteCategory', admin.deleteCategory);
+app.post('/deleteQuestion', admin.deleteQuestion);
+app.post('/edit_Category/:id', admin.edit_Category_id);
+app.post('/show_question/:id', admin.show_question_id);
+app.post('/show_question/:id', admin.show_question_id);
+app.post('/edit_question/:id', admin.edit_question_id);
+app.post('/editCategory/:id', admin.editCategory_id);
+app.post('/editQuestion_details/:id', admin.editQuestion_details_id);
+app.post('/addCateogrydetails', admin.addCateogrydetails);
+app.post('/addQuestion',admin.addQuestion);
+app.get('/viewCategory', admin.viewCategory);
+
+
+
+app.post('/user_login',user_det.check_login);
+app.post('/registration',user_det.registration);
+
+app.get('/entrypage',user_det.getAllquestions);
+//==================================================================
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
